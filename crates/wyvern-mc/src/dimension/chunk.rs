f1d652dc06c7a84f433f5353c3ed70d1bb35b597
akarahdev <endisticbutitsforgit@gmail.com>
2025-01-29 11:15:34 -0500

    feat: create Chunk struct for better api

--- crates/wyvern-mc/src/dimension/chunk.rs
+++ crates/wyvern-mc/src/dimension/chunk.rs
@@ -11,6 +11,48 @@ use crate::values::Position;
 use super::blocks::BlockState;
 
 #[derive(Clone, Debug)]
+pub struct Chunk {
+    sections: Vec<ChunkSection>,
+}
+
+impl Chunk {
+    pub fn new(sections: usize) -> Chunk {
+        let mut vec = Vec::with_capacity(sections);
+        for _ in 0..sections {
+            vec.push(ChunkSection::empty());
+        }
+        Chunk { sections: vec }
+    }
+
+    pub fn section_at_mut(&mut self, section: usize) -> Option<&mut ChunkSection> {
+        self.sections.get_mut(section)
+    }
+
+    pub fn set_block_at(&mut self, pos: Position<i32>, block: BlockState) {
+        let section_y = *pos.y() as usize / 16;
+        if let Some(section) = self.section_at_mut(section_y) {
+            section.set_block_at(
+                pos.map_coords(|x| *x as usize)
+                    .with_y(*pos.y() as usize % 16usize),
+                block,
+            );
+        }
+    }
+
+    pub fn get_block_at(&mut self, pos: Position<i32>) -> BlockState {
+        let section_y = *pos.y() as usize / 16;
+        if let Some(section) = self.section_at_mut(section_y) {
+            return section.get_block_at(
+                pos.map_coords(|x| *x as usize)
+                    .with_y(*pos.y() as usize % 16usize),
+            );
+        } else {
+            return BlockState::from_protocol_id(0);
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct ChunkSection {
     block_count: i16,
     blocks: [[[RegEntry<ProtocolState>; 16]; 16]; 16],
