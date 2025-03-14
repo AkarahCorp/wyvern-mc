9f5a86d90a533bd6ca2c3e1f7003536927d672b3
akarahdev <endisticbutitsforgit@gmail.com>
2025-03-13 20:43:45 -0400

    feat: rename wyvern-mc to wyvern-core, and wyvern-mc is now a public re-export for wyvern-core

---
+++ crates/wyvern-core/src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(try_blocks)]
+#![allow(clippy::type_complexity)]
+
+pub mod actors;
+pub mod blocks;
+pub mod components;
+pub mod dimension;
+pub mod entities;
+pub mod events;
+pub mod inventory;
+pub mod item;
+pub mod player;
+pub mod runtime;
+pub mod server;
+pub mod values;
+
+pub use wyvern_macros::*;
--- crates/wyvern-mc/src/lib.rs
+++ crates/wyvern-mc/src/lib.rs
@@ -1,17 +1,1 @@
-#![feature(try_blocks)]
-#![allow(clippy::type_complexity)]
-
-pub mod actors;
-pub mod blocks;
-pub mod components;
-pub mod dimension;
-pub mod entities;
-pub mod events;
-pub mod inventory;
-pub mod item;
-pub mod player;
-pub mod runtime;
-pub mod server;
-pub mod values;
-
-pub use wyvern_macros::*;
+pub use wyvern_core::*;
